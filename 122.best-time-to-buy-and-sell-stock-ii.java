2ef41a83089583eee7dae16beae26ae178e48fe9
Allen Feng <yfeng445@uwo.ca>
2024-09-24 15:40:52 -0400

    upd: 122. 感觉比121简单

---
+++ 122.best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int currPrice = prices[0];
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] > currPrice){
+                profit += (prices[i] - currPrice);
+            } 
+            currPrice = prices[i];
+        }
+        return profit;
+    }
+}
+
+// @lc code=end
+
