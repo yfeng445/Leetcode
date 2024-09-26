74d3bff9c2e9b4bdd42058f8bee3415256198e6f
Allen Feng <yfeng445@uwo.ca>
2024-09-26 14:40:17 -0400

    Create 729.My-Calener-I.java

---
+++ 729.My-Calener-I.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=729 lang=java
+ *
+ * [729] My Calendar I
+ */
+
+// @lc code=start
+import java.util.ArrayList;
+class MyCalendar {
+
+    ArrayList<int[2]> records;
+    
+    public MyCalendar() {
+        records = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        if(start<end) return false;
+        records.app
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
+// @lc code=end
+
