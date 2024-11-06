aa4b73ed7602b1bd109494c578054d178bb278f7
Allen Feng <yfeng445@uwo.ca>
2024-11-06 08:15:26 -0500

    upd. 3011

---
+++ 3011.find-if-array-can-be-sorted.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3011 lang=java
+ *
+ * [3011] Find if Array Can Be Sorted
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
