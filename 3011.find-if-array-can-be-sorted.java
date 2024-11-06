4a1851aa8df0bd1a0abc7ca8649b5c0326ace34d
Allen Feng <yfeng445@uwo.ca>
2024-11-06 09:24:29 -0500

    3011. Array操作

--- 3011.find-if-array-can-be-sorted.java
+++ 3011.find-if-array-can-be-sorted.java
@@ -5,9 +5,47 @@
  */
 
 // @lc code=start
+
+import java.util.Arrays;
+
 class Solution {
     public boolean canSortArray(int[] nums) {
-        
+        int[] sortedNums  = nums.clone();
+        Arrays.sort(sortedNums); // The compare result
+        for(int i = 0; i<nums.length-1; i++){
+            if(getSetbit(nums[i]) != getSetbit(nums[i+1])){
+                int j = i+1; 
+                int minAfter = nums[j];
+                int maxBefore = nums[i];
+                for(int k = 0; k<i; k++){
+                    if(nums[k]>maxBefore){
+                        maxBefore = nums[k];
+                    }
+                }
+                for(; j<nums.length; j++){
+                    if(nums[j]<minAfter){
+                        minAfter = nums[j]; 
+                    }
+                }
+                //System.out.println(minLocal);
+                if(maxBefore > minAfter){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public int getSetbit(int i){
+        int setBit = 0;
+        while(i!=0){
+            if(i%2 == 1){
+              setBit++;  
+            }
+            i = i/2;
+        }
+        return setBit;
     }
 }
 // @lc code=end
