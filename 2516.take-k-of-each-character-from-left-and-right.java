430c3375a075129fca42f60321d188ebfe85c297
Yuki <yfeng445@uwo.ca>
2024-11-20 10:39:36 -0500

    2516. sliding window, 难, 思路有 edge case错.

--- 2516.take-k-of-each-character-from-left-and-right.java
+++ 2516.take-k-of-each-character-from-left-and-right.java
@@ -9,10 +9,12 @@ class Solution {
     public int takeCharacters(String s, int k) {
         System.out.println(s);
         int countA = 0, countB = 0, countC = 0;
-        int left = 0, right = 0;
+        int left = 0, right = s.length()-1;
         int minute = 0;
-        while((countA<k||countB<k||countC<k)&&right<s.length()){
 
+        if(k == 0) return 0;
+        
+        while((left<=right)&&(countA<k||countB<k||countC<k)){
             if(s.charAt(left) == 'a'){
                 countA++; 
                 left++;
@@ -38,43 +40,46 @@ class Solution {
                 countC++;
                 right--;
                 minute+=1;
-            }  
+            } 
         }
-        if(countA<k||countB<k||countC<k) return -1;
+        if((left>=right)||(countA<k||countB<k||countC<k)) return -1;
+
         left --;
         right ++;
-        System.out.println("balance: " + minute +"minute" + ", Left: " + left + ", right: " + right);
-
+        System.out.println("balance: " + minute +" minute" + ", Left: " + left + ", right: " + right);
 
         while(true){
             int left0 = left, right0 = right;
             if(s.charAt(left) == 'a'&&countA>k){
                 left--;
                 countA--;
-                minute--;
             }else if(s.charAt(left) == 'b'&&countB>k){
                 left--;
                 countB--;
-                minute--;
             }else if(s.charAt(left) == 'c'&&countC>k){
                 left--;
                 countC--;
-                minute--;
             }else if(s.charAt(right) == 'a'&&countA>k){
                 right++;
                 countA--;
-                minute--;
             }else if(s.charAt(right) == 'b'&&countB>k){
                 right++;
                 countB--;
-                minute--;
             }else if(s.charAt(right) == 'c'&&countC>k){
                 right++;
                 countC--;
-                minute--;
             }
-            System.out.println("Minute: " + minute+", Left: " + left + ", Right: " + right + "\ncountA=" + countA + ", countB="+countB + ", countC=" + countC);
-            if(left0 == left && right0 == right) return minute;
+            
+            if(left0 == left && right0 == right){
+                System.out.println("Minute: " + minute+", Left: " + left + ", Right: " + right + "\ncountA=" + countA + ", countB="+countB + ", countC=" + countC);
+                return minute;
+            }else{
+                minute--; 
+                System.out.println("Minute: " + minute+", Left: " + left + ", Right: " + right + "\ncountA=" + countA + ", countB="+countB + ", countC=" + countC);
+               
+            }
+
+            
         }
     }
 }
