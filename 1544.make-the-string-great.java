72f4fce34214e4f78f2817afd06c3441bc5b5bae
Yuki <yfeng445@uwo.ca>
2025-01-13 14:30:23 -0500

    1544. Google L4

--- 1544.make-the-string-great.java
+++ 1544.make-the-string-great.java
@@ -5,9 +5,32 @@
  */
 
 // @lc code=start
+import java.util.Stack;
+
 class Solution {
     public String makeGood(String s) {
+        Stack<Character> result = new Stack<Character>();
+        for(int i = 0; i<s.length(); i++){
+            if(result.isEmpty()){
+                result.push(s.charAt(i));
+            }
+            else{
+                char top = result.peek();
+                if(top!=s.charAt(i)&&
+                (String.valueOf(top).toLowerCase().equals(String.valueOf(s.charAt(i)).toLowerCase()))){
+                    result.pop();
+                }
+                else{
+                    result.push(s.charAt(i));
+                }
+            }
+        }
         
+        StringBuilder sb = new StringBuilder();
+        for (Character c : result) {
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
 // @lc code=end
