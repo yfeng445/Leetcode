9f0d2a4188586bf83ea4a0dc723b7b1ebadd79ef
Yuki <yfeng445@uwo.ca>
2025-01-27 17:02:18 -0500

    283. 最笨的方法; 最快的方法

--- 283.move-zeroes.java
+++ 283.move-zeroes.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode id=283 lang=java
  *
@@ -5,9 +8,23 @@
  */
 
 // @lc code=start
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        
+        int[] tmp = new int[nums.length];
+        int pos = 0;
+        int counter = 0;
+        for(int i : nums){
+            if(i == 0) counter ++;
+            else{
+                tmp[pos] = i;
+                pos++;
+            }
+        }
+        for(int i = 0; i<counter; i++){
+            tmp[tmp.length-i-1] = 0;
+        }
+        System.arraycopy(tmp, 0, nums, 0, tmp.length);
     }
 }
 // @lc code=end
