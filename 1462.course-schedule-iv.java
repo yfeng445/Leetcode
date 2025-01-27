9806bf6fafa9ab6b8e73f56ec0301b9947b30016
Yuki <yfeng445@uwo.ca>
2025-01-27 10:31:26 -0500

    1462. 没做完 DFS 基本功不过关...

---
+++ 1462.course-schedule-iv.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1462 lang=java
+ *
+ * [1462] Course Schedule IV
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+
+        
+        return new ArrayList<>();
+    }    
+}
+
+class Node {
+    int value;
+    ArrayList<Node> prev;
+
+    TreeNode(int value) {
+        this.value = value;
+        this.prev = new ArrayList<>();
+    }
+}
+// @lc code=end
+
