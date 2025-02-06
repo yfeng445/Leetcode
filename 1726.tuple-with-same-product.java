3ca5dfd7cb40b76848290a958757600989deb3f0
Yuki <yfeng445@uwo.ca>
2025-02-06 10:51:41 -0500

    1726. 转化思路很妙, 有思路好做很多

--- 1726.tuple-with-same-product.java
+++ 1726.tuple-with-same-product.java
@@ -5,9 +5,45 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 class Solution {
     public int tupleSameProduct(int[] nums) {
-        
+        if(nums.length < 4){
+            return 0;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                if(map.get(nums[i]*nums[j]) == null){
+                    map.put(nums[i]*nums[j], 1);
+                }
+                else{
+                    map.put(nums[i]*nums[j], map.get(nums[i]*nums[j])+1);
+                }
+            }
+        }
+        int count = 0;
+        for(HashMap.Entry<Integer, Integer> en : map.entrySet()){
+            if(en.getValue()>1){
+                count +=get(en.getValue());
+            }
+        }
+
+        return count*8;
+    }
+
+    public int get(int i){
+        i--;
+        int rtn = 1;
+        while(i != 1){
+            rtn += i;
+            i--;
+        }
+        return rtn;
     }
 }
 // @lc code=end
