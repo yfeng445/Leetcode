b7f623ab1ba0f15bd2406387c931f97f220456f7
Yuki <yfeng445@uwo.ca>
2025-02-11 12:06:36 -0500

    203. 思路很简单, 但是被edge case卡了好久...

---
+++ 203.remove-linked-list-elements.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=203 lang=java
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        // null
+        if(head == null) return null;
+
+        // head is val
+        while(head.next!=null){
+            if(head.val!=val) break;
+            else head = head.next;
+        }
+        if(head.val == val) return null;
+
+        if(head.next == null){
+            if(head.val == val)return null;
+            else return head;
+        }  
+        
+
+        ListNode cpHead = head;
+
+        while(head.next != null){
+            if(head.next.next == null){
+                if(head.next.val == val) head.next = null;
+                break;
+            }
+            else{
+                if(head.next.val == val){
+                    head.next = head.next.next;
+                }
+            }
+            if(head.next.val != val){
+                head = head.next;   
+            }
+            
+             
+        }
+        
+        return cpHead;
+    }
+}
+// @lc code=end
+
