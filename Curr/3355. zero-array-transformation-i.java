a2aec402c549caa9a6db08b3d4ce09c60c77080c
Yuki <yfeng445@uwo.ca>
2025-05-20 15:07:22 +0800

    3355. diffArr, 想到就能过

---
+++ Curr/3355. zero-array-transformation-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int[] diffArr = new int[nums.length];
+
+        for(int[] query : queries){
+            diffArr[query[0]]++;
+            if(query[1] != diffArr.length-1) diffArr[query[1]+1]--;
+        }
+
+        if(nums[0] > diffArr[0]) return false;
+        int transitionVal = diffArr[0];
+        nums[0] -= diffArr[0];
+
+        for(int i = 1; i<nums.length; i++){
+            transitionVal += diffArr[i];
+            nums[i] -= transitionVal;
+            if(nums[i] > 0) return false;
+        }
+
+        return true;
+    }
+}
