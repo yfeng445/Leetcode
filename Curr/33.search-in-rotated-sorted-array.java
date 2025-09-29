460bf8fe1fa97d74a52ede670ccaafcbb04e15e2
Yuki <yfeng445@uwo.ca>
2025-09-29 15:53:40 -0400

    33. 为什么On能过, 甚至是最优解...

---
+++ Curr/33.search-in-rotated-sorted-array.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i = 0; i<nums.length; i++) if(nums[i] == target) return i;
+        return -1 ;
+    }
+}
+// @lc code=end
+
