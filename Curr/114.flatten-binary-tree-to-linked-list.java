c8b14ef2db9b320ff71759af04e6a2e1e41944f2
Yuki <yfeng445@uwo.ca>
2026-01-21 15:34:54 -0500

    114. 为什么这么简单的题能卡这么久?

---
+++ Curr/114.flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=114 lang=java
+ *
+ * [114] Flatten Binary Tree to Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        while(root != null){
+            TreeNode tmp = root.right;
+            root.right = root.left;
+            root.left = null;
+            getRight(root).right = tmp;
+            root = root.right;
+        }
+    }
+
+    public TreeNode getRight(TreeNode root){
+        while(root.right!=null) root = root.right;
+        return root;
+    }
+}
+// @lc code=end
+
