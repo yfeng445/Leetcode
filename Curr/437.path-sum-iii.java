0dec0a642f732edd0c735bb24b7f1b8c330c1626
Yuki <yfeng445@uwo.ca>
2026-01-22 10:35:36 -0500

    upd. 437. 要想明白...

---
+++ Curr/437.path-sum-iii.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=437 lang=java
+ *
+ * [437] Path Sum III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        return countFrom(root, (long)targetSum) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+    }
+
+    private int countFrom(TreeNode node, long sum) {
+        if (node == null) return 0;
+        return ((node.val == sum) ? 1 : 0) + countFrom(node.left, sum - node.val) + countFrom(node.right, sum - node.val);
+    }
+}
+
+// @lc code=end
+
